4253edbe2254ec312824a51f5339c0e3e27b6e48
Alba Lopes <alba.lopes@ifrn.edu.br>
2018-09-26 09:34:00 -0300

    Aula de 26/09/2018 - Introdução a Javascript

---
+++ exemplo_js2018/aula01/minhasfuncoes.js
@@ -0,0 +1,17 @@
+function trocarConteudo(){
+    document.getElementById('meuparagrafo').innerHTML = 'A mágica já aconteceu!';
+    document.getElementById('meuparagrafo').style.color = "red";
+}
+
+function trocarCor(){
+    document.getElementById('olamundo').style.backgroundColor='yellow';
+    alert('A cor foi alterada!');
+    console.log('Eu estou escrevendo aqui');
+
+    
+}
+
+function trocarImagem(){
+    document.getElementById("figuraola").src="https://cdn.pixabay.com/photo/2012/04/01/17/14/earth-23593_960_720.png"
+
+}
